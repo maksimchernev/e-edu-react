ad8e2edd57a3e63c97e82903ed87d5564f010cfa
Lina <lida2305@inbox.ru>
2020-11-19 15:55:06 +0300

    authorization update

---
+++ frontend/src/components/lk.js
@@ -0,0 +1,10 @@
+import React, { useEffect, useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export const  PersonalAcc =() => {
+     return(
+        <div>
+            <h1> Personal Account</h1>
+        </div>
+     )
+}
---
+++ frontend/src/pages/PersonalAccount.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { PersonalAcc } from '../components/lk'
+
+export const PersonalAccount = () => {
+
+    return (
+        <div>
+          <PersonalAcc/>
+        </div>
+    )
+}
