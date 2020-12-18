5b68bbc7f4ef456c64251753c5b47687bc759d4f
Kirill Prytin <indygame666@gmail.com>
2020-12-18 13:52:12 +0300

    card courses functionality minor fixes

--- frontend/src/components/coursecard.js
+++ frontend/src/components/coursecard.js
@@ -7,30 +7,29 @@ export const CourseCard =( { cardInfo }) => {
   var maxRowCount = 3;
 
   return(
-
-  <tbody>
+      <div>
         { cardInfo.map( (card) =>{
 
       return(
-        <tr key = {card._id}>
+        <div key = {card._id}>
   
 					<FlippingCard className="card" style={{border: "2px solid #003399"}}>
                         <FlippingCardBack>
-                       <h3>Course field:</h3> <td>{card.field}</td>
-                       <h4>Course difficulty:</h4><td>{card.difficulty}</td>
+                       <h3>Course field:</h3><h6>{card.field}</h6>
+                       <h4>Course difficulty:</h4><h6>{card.difficulty}</h6>
                         </FlippingCardBack>
 
                         <FlippingCardFront>
                         <h3>Course Name:</h3>
-                        <td>{card.name}</td>
+                        <h6>{card.name}</h6>
                         </FlippingCardFront>
             </FlippingCard>     
         
         
-        </tr>
+        </div>
       )
         })
         }
-      </tbody>
+      </div>
   )
 }
