71756cb8e2c5ee75f8342c917783c1210f208d2e
Lina <lida2305@inbox.ru>
2021-01-27 16:37:11 +0300

    add icons in footer for soc networks

--- frontend/src/components/footer.js
+++ frontend/src/components/footer.js
@@ -2,11 +2,21 @@ import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Button, Navbar, Nav} from 'react-bootstrap';
 import '../сss/footer.css'
+import fb from '../images/footer/Facebook - Negative.png'
+import inst from '../images/footer/Instagram - Negative.png'
+import tw from '../images/footer/Twitter - Negative.png'
+import vk from '../images/footer/vk.png'
+import yout from '../images/footer/YouTube - Negative.png'
 
 export const Footer =() => {
 	return(
     <footer className="footer-grid ">
       <Navbar expand="lg">
+            <a href="#"><img className="soc-net" src={fb} /></a>
+            <a href="#"><img className="soc-net" src={inst} /></a>
+            <a href="#"><img className="soc-net" src={tw} /></a>
+            <a href="#"><img className="soc-net" src={vk} /></a>
+            <a href="#"><img className="soc-net" src={yout} /></a>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="ml-auto color-text">
